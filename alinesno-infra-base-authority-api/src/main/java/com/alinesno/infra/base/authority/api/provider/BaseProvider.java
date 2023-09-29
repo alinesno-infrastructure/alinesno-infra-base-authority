653aa71b09774e2887520070eff7ecca50939ec8
switchII <luoandon@gmail.com>
2023-09-30 02:16:43 +0800

    feat:添加对外接口规划

---
+++ alinesno-infra-base-authority-api/src/main/java/com/alinesno/infra/base/authority/api/provider/BaseProvider.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.authority.api.provider;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class BaseProvider {
+}
---
+++ alinesno-infra-base-authority-api/src/main/java/com/alinesno/infra/base/authority/api/provider/OrganizeProviderController.java
@@ -0,0 +1,47 @@
+package com.alinesno.infra.base.authority.api.provider;
+
+import com.alinesno.infra.base.authority.api.dto.DeptDto;
+import com.alinesno.infra.base.authority.api.dto.ManagerApplicationDto;
+import com.alinesno.infra.base.authority.api.dto.ManagerTenantDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/api/base/authority/organize")
+public class OrganizeProviderController extends BaseProvider {
+
+    /**
+     * 查询租户
+     *
+     * @param ids
+     * @return
+     */
+    public List<ManagerTenantDto> findTenantEntityByIds(List<String> ids){
+        return null ;
+    }
+
+    /**
+     * 通过ids查询应用
+     *
+     * @param ids
+     * @return
+     */
+    public List<ManagerApplicationDto> findApplicationEntityByIds(List<String> ids){
+        return null ;
+    }
+
+    /**
+     * 查询应用部门
+     *
+     * @param applicationId
+     * @param parentDeptId
+     * @return
+     */
+    public List<DeptDto> findOrg(String applicationId, String parentDeptId){
+        return null ;
+    }
+}
