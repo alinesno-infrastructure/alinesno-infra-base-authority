828c0d6c9cec66a19e63b271ff437bb2a32ded98
switchII <luoandon@gmail.com>
2023-09-29 10:42:22 +0800

    feat:集成SSO服务

--- alinesno-infra-base-authority-boot/src/main/java/com/alinesno/infra/base/authority/BaseAuthorityApplication.java
+++ alinesno-infra-base-authority-boot/src/main/java/com/alinesno/infra/base/authority/BaseAuthorityApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.base.authority;
 
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.base.authority.mapper")
 @SpringBootApplication
 public class BaseAuthorityApplication {
