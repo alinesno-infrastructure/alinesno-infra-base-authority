0846962f81ae998a7fa1941cbf507f8666b6b9f1
周健(James) <jamesAip@gmail.com>
2024-08-28 21:49:00 -0400

    新增 EnterpriseSeoMapper 接口文件。

---
+++ alinesno-infra-base-authority-meta/src/main/java/com/alinesno/infra/base/authority/meta/mapper/EnterpriseSeoMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.authority.meta.mapper;
+
+import com.alinesno.infra.base.authority.meta.entity.EnterpriseSeoEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface EnterpriseSeoMapper extends IBaseMapper<EnterpriseSeoEntity> {
+}
