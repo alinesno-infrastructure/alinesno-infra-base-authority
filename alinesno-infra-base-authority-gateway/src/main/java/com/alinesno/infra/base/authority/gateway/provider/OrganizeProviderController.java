849ad63d462a4b891e1c10d0bdb8584668638c49
周健(James) <jamesAip@gmail.com>
2024-08-28 22:40:00 -0400

    改进了OrganizeProviderController类的方法，增加了查询组织信息和更新组织信息的功能。

--- alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/provider/OrganizeProviderController.java
+++ alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/provider/OrganizeProviderController.java
@@ -1,47 +1,57 @@
 package com.alinesno.infra.base.authority.gateway.provider;
 
-import com.alinesno.infra.base.authority.gateway.dto.DeptDto;
-import com.alinesno.infra.base.authority.gateway.dto.ManagerProjectDto;
-import com.alinesno.infra.base.authority.gateway.dto.ManagerGroupDto;
+import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.base.authority.api.OrganizationDto;
+import com.alinesno.infra.base.authority.entity.OrganizationEntity;
+import com.alinesno.infra.base.authority.service.IOrganizationService;
+import com.alinesno.infra.common.facade.response.R;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/v1/api/base/authority/organize")
 public class OrganizeProviderController extends BaseProvider {
 
+    @Autowired
+    private IOrganizationService organizationService ;
+
     /**
-     * 查询机构
-     *
-     * @param ids
+     * 查询组织信息
      * @return
      */
-    public List<ManagerGroupDto> findTenantEntityByIds(List<String> ids){
-        return null ;
+    @GetMapping("/findOrg")
+    public R<OrganizationDto> findOrg(long id){
+
+        OrganizationEntity entity = organizationService.getById(id) ;
+        Assert.notNull(entity , "组织信息不存在");
+
+        OrganizationDto dto = new OrganizationDto() ;
+        BeanUtils.copyProperties(entity,dto);
+
+        return R.ok(dto);
     }
 
     /**
-     * 通过ids查询应用
-     *
-     * @param ids
-     * @return
+     * 更新组织信息
      */
-    public List<ManagerProjectDto> findApplicationEntityByIds(List<String> ids){
-        return null ;
+    @PostMapping("/updateOrg")
+    public R<OrganizationDto> updateOrg(@RequestBody OrganizationDto dto){
+
+        OrganizationEntity entity = new OrganizationEntity() ;
+        Assert.notNull(dto.getId(),"id不能为空") ;
+
+        BeanUtils.copyProperties(dto,entity);
+
+        entity.setOrganizationId(IdUtil.getSnowflakeNextIdStr());
+        entity.setDoorplateNumber(IdUtil.nanoId(8));
+
+        boolean b = organizationService.saveOrUpdate(entity);
+
+        return b?R.ok():R.fail();
     }
 
-    /**
-     * 查询应用部门
-     *
-     * @param applicationId
-     * @param parentDeptId
-     * @return
-     */
-    public List<DeptDto> findOrg(String applicationId, String parentDeptId){
-        return null ;
-    }
 }
