eacefa85a3ff167a92cf1029c7eb32355e9cdbca
周健(James) <jamesAip@gmail.com>
2024-08-28 20:11:00 -0400

    新增Base64压缩工具类测试用例，包含在线图片压缩与下载功能验证。

---
+++ alinesno-infra-base-authority-core/src/test/java/com/alinesno/infra/base/authority/core/utils/Base64CompressUtilsTets.java
@@ -0,0 +1,42 @@
+package com.alinesno.infra.base.authority.core.utils;
+
+import cn.hutool.core.io.FileTypeUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Console;
+import org.junit.Test;
+
+import java.io.File;
+
+public class Base64CompressUtilsTets {
+
+    @Test
+    public void test() throws Exception {
+        //在线图片地址
+        String imgPath = "https://pss.bdstatic.com/static/superman/img/logo/logo_redBlue-0a7c20fcaa.png";
+
+        //在线压缩
+        String ste2= ImageUtils.getImageAsBase64(imgPath) ;
+        System.out.println(ste2);
+
+        imgPath = "http://portal.infra.linesno.com/logo.png" ;
+
+        //在线压缩
+        ste2= ImageUtils.getImageAsBase64(imgPath) ;
+        System.out.println(ste2);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        String imgPath = "https://pss.bdstatic.com/static/superman/img/logo/logo_redBlue-0a7c20fcaa.png";
+
+        String de = ImageUtils.downloadImage(imgPath) ;
+        System.out.println(de);
+
+        File file = FileUtil.file(de);
+        String type = FileTypeUtil.getType(file);
+
+        //输出 jpg则说明确实为jpg文件
+        Console.log(type);
+    }
+
+}
