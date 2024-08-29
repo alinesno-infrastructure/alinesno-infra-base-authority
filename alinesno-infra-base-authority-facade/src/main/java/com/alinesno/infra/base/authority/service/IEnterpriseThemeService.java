523e597fd9aeaddcc70f93bc5376538b821b21ea
周健(James) <jamesAip@gmail.com>
2024-08-28 23:12:00 -0400

    更新:alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/service/IEnterpriseThemeService.java

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/service/IEnterpriseThemeService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.base.authority.service;
-
-import com.alinesno.infra.base.authority.entity.EnterpriseThemeEntity;
-import com.alinesno.infra.common.facade.services.IBaseService;
-
-public interface IEnterpriseThemeService extends IBaseService<EnterpriseThemeEntity> {
-}
