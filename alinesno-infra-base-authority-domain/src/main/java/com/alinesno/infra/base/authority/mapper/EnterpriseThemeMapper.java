a4b2171abf36ca451c2fcc1d949316a4d6063b90
周健(James) <jamesAip@gmail.com>
2024-08-29 06:59:00 -0400

    更新:alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/mapper/EnterpriseThemeMapper.java

--- alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/mapper/EnterpriseThemeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.base.authority.mapper;
-
-import com.alinesno.infra.base.authority.entity.EnterpriseThemeEntity;
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnterpriseThemeMapper extends IBaseMapper<EnterpriseThemeEntity> {
-}
