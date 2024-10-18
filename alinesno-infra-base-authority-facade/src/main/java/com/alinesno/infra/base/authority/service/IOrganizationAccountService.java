aa2159bfa7354e2929fdf74dba66ac905ca5e629
周健(James) <jamesAip@gmail.com>
2024-10-19 00:12:41 +0800

    refactor(IOrganizationAccountService): 移除不必要的方法

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/service/IOrganizationAccountService.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/service/IOrganizationAccountService.java
@@ -1,12 +1,7 @@
 package com.alinesno.infra.base.authority.service;
 
-import com.alinesno.infra.base.authority.api.OrganizationDto;
 import com.alinesno.infra.base.authority.entity.OrganizationAccountEntity;
-import com.alinesno.infra.base.authority.entity.OrganizationEntity;
-import com.alinesno.infra.base.authority.entity.OrganizationLogEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
-
-import java.util.List;
 
 /**
  * <p>
@@ -19,19 +14,5 @@ import java.util.List;
 
 public interface IOrganizationAccountService extends IBaseService<OrganizationAccountEntity> {
 
-    /**
-     * 查询账号
-     * @param id
-     * @param orgId
-     * @return
-     */
-    OrganizationAccountEntity getByAccountIdAndOrgId(Long id, long orgId);
-
-    /**
-     * 查询账号所在组织列表
-     * @param id
-     * @return
-     */
-    List<OrganizationDto> listByAccountId(Long id);
 
 }
