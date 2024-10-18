0578b0155d7226539d979e647a9f4e3a0c6c93ad
林晓(Lily) <lilyAip@gmail.com>
2024-10-19 00:12:41 +0800

    refactor: 重构 auth.js 文件，将存储方式从 Cookies 更改为 localStorage

--- alinesno-infra-base-authority-ui/src/utils/auth.js
+++ alinesno-infra-base-authority-ui/src/utils/auth.js
@@ -1,20 +1,41 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
+// const TokenKey = 'Admin-Token'
 
-export function getToken() {
-  return Cookies.get(TokenKey)
-}
+// export function getToken() {
+//   return Cookies.get(TokenKey)
+// }
 
-export function getSaToken() {
-  return localStorage.getItem('satoken');
-}
+// // export function getSaToken() {
+// //   return localStorage.getItem('satoken');
+// // }
 
-export function setToken(token) {
-  return Cookies.set(TokenKey, token)
-}
+// export function setToken(token) {
+//   return Cookies.set(TokenKey, token)
+// }
 
-export function removeToken() {
-  Cookies.remove(TokenKey)
-  localStorage.removeItem('satoken');
-}
+// export function removeToken() {
+//   Cookies.remove(TokenKey)
+//   localStorage.removeItem('satoken');
+// }
+
+const TokenKey = 'Admin-Token';
+
+// 获取token
+export const getToken = () => {
+  return localStorage.getItem(TokenKey);
+};
+
+// 设置token
+export const setToken = (access_token) => {
+  if (typeof access_token === 'string') {
+    localStorage.setItem(TokenKey, access_token);
+  } else {
+    throw new Error('Access token must be a string');
+  }
+};
+
+// 移除token
+export const removeToken = () => {
+  localStorage.removeItem(TokenKey);
+};
