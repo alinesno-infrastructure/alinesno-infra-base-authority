0b2b66042d091acce6a5bb03c0f5824ce3e23d2d
周健(James) <jamesAip@gmail.com>
2024-10-19 00:12:41 +0800

    feat:在 `AccountProviderController` 中添加组织服务及注册功能完善

--- alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/provider/AccountProviderController.java
+++ alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/provider/AccountProviderController.java
@@ -1,10 +1,13 @@
 package com.alinesno.infra.base.authority.gateway.provider;
 
 import cn.hutool.crypto.digest.BCrypt;
+import com.alinesno.infra.base.authority.api.OrganizationDto;
 import com.alinesno.infra.base.authority.entity.ManagerAccountEntity;
 import com.alinesno.infra.base.authority.gateway.dto.ManagerAccountDto;
 import com.alinesno.infra.base.authority.gateway.dto.ManagerResourceDto;
 import com.alinesno.infra.base.authority.service.IManagerAccountService;
+import com.alinesno.infra.base.authority.service.IOrganizationService;
+import com.alinesno.infra.common.facade.response.R;
 import com.alinesno.infra.common.web.adapter.dto.LoginBodyDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -12,56 +15,62 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/v1/api/base/authority/account")
 public class AccountProviderController extends BaseProvider {
 
     @Autowired
-    private IManagerAccountService managerAccountService ;
+    private IManagerAccountService managerAccountService;
+
+    @Autowired
+    private IOrganizationService organizationService;
 
     /**
      * 用户注册信息，用于前端业务用户注册
+     *
      * @param dto
      * @return
      */
     @PostMapping("/registerAccount")
-    public ManagerAccountDto registerAccount(@RequestBody ManagerAccountDto dto){
+    public ManagerAccountDto registerAccount(@RequestBody ManagerAccountDto dto) {
 
-        String loginName = dto.getLoginName() ;
-        String password = BCrypt.hashpw(dto.getPassword()) ;  // 密钥加密
-        String phone = dto.getPhone() ;
+        String loginName = dto.getLoginName();
+        String password = BCrypt.hashpw(dto.getPassword());  // 密钥加密
+        String phone = dto.getPhone();
 
-        ManagerAccountDto b = managerAccountService.registAccount(loginName , password , phone) ;
-        log.debug("registerAccount = {}" , b);
+        ManagerAccountDto b = managerAccountService.registAccount(loginName, password, phone);
+        log.debug("registerAccount = {}", b);
 
-        return b ;
+        return b;
     }
 
     /**
      * 用户登陆
+     *
      * @return
      */
     @PostMapping("/login")
-    public ManagerAccountDto login(@RequestBody LoginBodyDto loginBodyDto){
+    public ManagerAccountDto login(@RequestBody LoginBodyDto loginBodyDto) {
 
-        ManagerAccountDto dto = managerAccountService.loginAccount(loginBodyDto.getUsername() , loginBodyDto.getPassword()) ;
-        log.debug("login account = {}" , dto);
+        ManagerAccountDto dto = managerAccountService.loginAccount(loginBodyDto.getUsername(), loginBodyDto.getPassword());
+        log.debug("login account = {}", dto);
 
-        return dto ;
+        return dto;
     }
 
     /**
      * 查询用户菜单
      *
      * @param projectCode
-     * @param userId
+     * @param accountId
      * @return
      */
-    public ManagerResourceDto findMenusByProjectCode(String projectCode, String userId) {
-        return null ;
+    public ManagerResourceDto findMenusByProjectCode(String projectCode, String accountId) {
+        return null;
     }
-
 
     /**
      * 通过用户名查询用户
@@ -71,15 +80,46 @@ public class AccountProviderController extends BaseProvider {
      */
     @GetMapping("/findByLoginName")
     public ManagerAccountDto findByLoginName(String loginName) {
-        Assert.hasLength(loginName , "用户名称为空");
+        Assert.hasLength(loginName, "用户名称为空");
 
-        ManagerAccountEntity e = managerAccountService.findByLoginName(loginName) ;
+        ManagerAccountEntity e = managerAccountService.findByLoginName(loginName);
 
-        ManagerAccountDto dto = new ManagerAccountDto() ;
-        BeanUtils.copyProperties(e , dto);
+        ManagerAccountDto dto = new ManagerAccountDto();
+        BeanUtils.copyProperties(e, dto);
         dto.setPassword(null);
 
-        return dto ;
+        return dto;
+    }
+
+    /**
+     * 获取用户组织
+     */
+    @GetMapping("/findOrg")
+    public R<List<OrganizationDto>> findOrg(long accountId) {
+        List<OrganizationDto> list = organizationService.listByAccountId(accountId);
+        return R.ok(list);
+    }
+
+    /**
+     * 加入组织
+     */
+    @GetMapping("/joinOrg")
+    public R<String> joinOrg(long accountId, long orgId) {
+        organizationService.joinOrg(accountId, orgId);
+        return R.ok();
+    }
+
+    /**
+     * 退出组织
+     *
+     * @param accountId
+     * @param orgId
+     * @return
+     */
+    @GetMapping("/quitOrg")
+    public R<String> quitOrg(long accountId, long orgId) {
+        organizationService.quitOrg(accountId, orgId);
+        return R.ok();
     }
 
 }
