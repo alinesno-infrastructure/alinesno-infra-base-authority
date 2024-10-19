ef7ee54ff9baaf09ae9d2fa801056406f1c7f63e
李娜(Allen) <allenAip@gmail.com>
2024-10-19 14:50:10 +0800

    移除不必要的日志导入，简化代码。

--- alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerAccountRoleServiceImpl.java
+++ alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerAccountRoleServiceImpl.java
@@ -10,8 +10,6 @@ import com.alinesno.infra.common.facade.wrapper.mybatis.WrapperGenerator;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
