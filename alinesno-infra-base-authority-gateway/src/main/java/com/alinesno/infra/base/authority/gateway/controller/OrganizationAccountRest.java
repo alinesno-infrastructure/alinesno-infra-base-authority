68b66e7b36a49a3eee6dd81f58a354932602e1d8
李娜(Allen) <allenAip@gmail.com>
2024-10-19 14:50:11 +0800

    优化 OrganizationAccountRest 控制器导入，移除未使用的导入

--- alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/controller/OrganizationAccountRest.java
+++ alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/controller/OrganizationAccountRest.java
@@ -1,17 +1,13 @@
 package com.alinesno.infra.base.authority.gateway.controller;
 
-import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.base.authority.entity.ManagerAccountEntity;
 import com.alinesno.infra.base.authority.entity.OrganizationAccountEntity;
-import com.alinesno.infra.base.authority.entity.OrganizationEntity;
 import com.alinesno.infra.base.authority.gateway.dto.ManagerAccountDto;
 import com.alinesno.infra.base.authority.service.IManagerAccountService;
 import com.alinesno.infra.base.authority.service.IOrganizationAccountService;
-import com.alinesno.infra.base.authority.service.IOrganizationService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
-import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -21,7 +17,9 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
