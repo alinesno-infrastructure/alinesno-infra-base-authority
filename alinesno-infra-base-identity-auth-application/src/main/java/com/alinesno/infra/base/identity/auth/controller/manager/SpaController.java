959703724c317d57d9d5df5274bf9b38b7b8b344
李娜(Allen) <allenAip@gmail.com>
2024-10-19 14:50:12 +0800

    移除不必要的 @RequestMapping 注解

--- alinesno-infra-base-identity-auth-application/src/main/java/com/alinesno/infra/base/identity/auth/controller/manager/SpaController.java
+++ alinesno-infra-base-identity-auth-application/src/main/java/com/alinesno/infra/base/identity/auth/controller/manager/SpaController.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.base.identity.auth.controller.manager;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
