0a6003ad84f50fc45e4be2011909437f7f854417
李娜(Allen) <allenAip@gmail.com>
2024-10-19 14:50:11 +0800

    refactor: 移除 ManagerProjectAccountServiceImpl 中不必要的导入和注释。

--- alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerProjectAccountServiceImpl.java
+++ alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerProjectAccountServiceImpl.java
@@ -1,20 +1,11 @@
 package com.alinesno.infra.base.authority.service.impl;
 
-import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.base.authority.entity.ManagerProjectAccountEntity;
-import com.alinesno.infra.base.authority.entity.ManagerProjectEntity;
-import com.alinesno.infra.base.authority.enums.SystemInnerEnums;
 import com.alinesno.infra.base.authority.mapper.ManagerProjectAccountMapper;
 import com.alinesno.infra.base.authority.service.IManagerProjectAccountService;
-import com.alinesno.infra.base.authority.service.IManagerProjectService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-import java.util.List;
 
 /**
  * <p>
