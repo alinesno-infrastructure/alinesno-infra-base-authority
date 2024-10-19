47c3d403058c103ba1246f1f3961343cb8f4015a
李娜(Allen) <allenAip@gmail.com>
2024-10-19 14:50:11 +0800

    refactor: 优化 ManagerResourceDto 类，简化属性并添加 ToString 注解。

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/ManagerResourceDto.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/ManagerResourceDto.java
@@ -1,11 +1,9 @@
 package com.alinesno.infra.base.authority.gateway.dto;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import java.awt.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,71 +13,46 @@ import java.util.List;
  * 这些属性包括资源的名称、链接、图标、父资源ID、组件、资源排序、菜单类型、打开目标、权限、权限脚本、可见性和子资源。
  * 该实体用于在系统中表示和管理各种资源，如菜单、按钮等。
  *
- * @author luoxiaodong 
+ * @author luoxiaodong
  * @version 1.0.0
  */
+@ToString
 @Data
 @NoArgsConstructor
 public class ManagerResourceDto implements Serializable {
 
-	private long id ; // 资源ID
-	private String resourceName; // 资源的名称
-	private String resourceLink; // 资源的链接地址
-	private String resourceIcon; // 资源的图标
-	private String component; // 资源对应的组件
-	private String menuType ; // 菜单类型，默认为0，表示菜单项
-	private String openTarget; // 资源打开的方式，如新窗口、当前窗口等
-	private String permission; // 资源的权限标识
-	private String permissionScript; // 资源的权限脚本
-	private String visible; // 资源的可见性，控制资源是否在界面上显示
-	private Long projectId; // 资源所属的项目ID
-	private Long resourceParent ; // 资源的父资源ID，0表示顶级资源
-	private Integer resourceOrder; // 资源的排序值
-	private List<ManagerResourceDto> subResource = new ArrayList<>(); // 资源的子资源列表，用于表示资源的层级关系
+    private String name; // 菜单名称
+    private String path; // 菜单路径
+    private boolean hidden; // 是否隐藏菜单
+    private String redirect; // 重定向路径
+    private String component; // 组件路径
+    private boolean alwaysShow; // 是否总是显示
+    private Meta meta; // 菜单元信息
+    private String perms ; // 权限标识
+    private List<ManagerResourceDto> children = new ArrayList<>(); // 子菜单列表
 
-	public ManagerResourceDto(String resourceLink, String visible, String component, String menuType, Meta meta, List<ManagerResourceDto> subResource) {
-		this.resourceLink = resourceLink;
-		this.visible = visible;
-		this.component = component;
-		this.menuType = menuType;
+    @Data
+    public static class Meta {
+        private String title; // 菜单标题
+        private String icon; // 菜单图标
+        private boolean noCache; // 是否不缓存
+        private String link; // 链接
 
-		this.resourceName = meta.getTitle();
-		this.resourceIcon = meta.getIcon();
-
-		this.subResource = subResource;
-	}
-
-	@Data
-	public static class Meta {
-		private String title;
-		private String icon;
-
-		public Meta(String title, String icon) {
-			this.title = title;
-			this.icon = icon;
-		}
-	}
-
-	/**
-	 * 快速从 dto 转换成 entity
-	 * 
-	 * @return dto 对应的 entity
-	 */
-	public ManagerResourceDto mapToDto() {
-		ObjectMapper toDtoMapper = new ObjectMapper();
-		toDtoMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		return toDtoMapper.convertValue(this, ManagerResourceDto.class);
-	}
-
-	/**
-	 * 快速从 entity 转换成 dto
-	 * 
-	 * @return dto 对应的 entity
-	 */
-	public ManagerResourceDto fromDto(ManagerResourceDto entity) {
-		ObjectMapper fromDtoMapper = new ObjectMapper();
-		fromDtoMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		return fromDtoMapper.convertValue(entity, ManagerResourceDto.class);
-	}
+        /**
+         * 构造方法
+         * 创建一个具有基本属性的菜单元信息对象。
+         *
+         * @param title 菜单标题
+         * @param icon 菜单图标
+         * @param noCache 是否不缓存
+         * @param link 链接
+         */
+        public Meta(String title, String icon, boolean noCache, String link) {
+            this.title = title;
+            this.icon = icon;
+            this.noCache = noCache;
+            this.link = link;
+        }
+    }
 
 }
