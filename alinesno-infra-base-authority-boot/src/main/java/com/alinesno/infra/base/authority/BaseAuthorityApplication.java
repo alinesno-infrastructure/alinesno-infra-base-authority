42ceedcc45c0beb425eba481d7efbcaafa74229f
周健(James) <jamesAip@gmail.com>
2024-10-21 00:09:43 +0800

    优化 BaseAuthorityApplication 启动方法的代码格式

--- alinesno-infra-base-authority-boot/src/main/java/com/alinesno/infra/base/authority/BaseAuthorityApplication.java
+++ alinesno-infra-base-authority-boot/src/main/java/com/alinesno/infra/base/authority/BaseAuthorityApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaseAuthorityApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BaseAuthorityApplication.class, args);
 	}
 
