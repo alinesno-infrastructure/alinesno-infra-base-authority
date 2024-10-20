9093215685d12bd5690312db9533eea020ff8761
周健(James) <jamesAip@gmail.com>
2024-10-21 00:09:44 +0800

    remove ManagerResourceDto内部类Meta及其构造方法

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/ManagerResourceDto.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/ManagerResourceDto.java
@@ -31,28 +31,4 @@ public class ManagerResourceDto implements Serializable {
     private String perms ; // 权限标识
     private List<ManagerResourceDto> children = new ArrayList<>(); // 子菜单列表
 
-    @Data
-    public static class Meta {
-        private String title; // 菜单标题
-        private String icon; // 菜单图标
-        private boolean noCache; // 是否不缓存
-        private String link; // 链接
-
-        /**
-         * 构造方法
-         * 创建一个具有基本属性的菜单元信息对象。
-         *
-         * @param title 菜单标题
-         * @param icon 菜单图标
-         * @param noCache 是否不缓存
-         * @param link 链接
-         */
-        public Meta(String title, String icon, boolean noCache, String link) {
-            this.title = title;
-            this.icon = icon;
-            this.noCache = noCache;
-            this.link = link;
-        }
-    }
-
 }
