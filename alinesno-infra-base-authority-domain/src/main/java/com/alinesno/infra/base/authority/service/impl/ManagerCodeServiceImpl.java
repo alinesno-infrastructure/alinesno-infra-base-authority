163926b4d3ab76e47df34327ceacfae994924dbd
李伟(Michael) <michaelAip@gmail.com>
2024-10-21 20:49:26 +0800

    改进 `codeListByType` 方法，增加数据范围查询条件

--- alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerCodeServiceImpl.java
+++ alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerCodeServiceImpl.java
@@ -1,9 +1,11 @@
 package com.alinesno.infra.base.authority.service.impl;
 
 import com.alinesno.infra.base.authority.entity.ManagerCodeEntity;
+import com.alinesno.infra.base.authority.enums.DataSourceScope;
 import com.alinesno.infra.base.authority.mapper.ManagerCodeMapper;
 import com.alinesno.infra.base.authority.service.IManagerCodeService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -22,8 +24,14 @@ import java.util.List;
 public class ManagerCodeServiceImpl extends IBaseServiceImpl<ManagerCodeEntity, ManagerCodeMapper> implements IManagerCodeService {
 
 	@Override
-	public List<ManagerCodeEntity> codeListByType(String type, String applicationId) { 
-		return null;
+	public List<ManagerCodeEntity> codeListByType(String type, String applicationId) {
+
+		// 查询公共参数(默认查询公共参数)
+		LambdaQueryWrapper<ManagerCodeEntity> query = new LambdaQueryWrapper<>();
+		query.eq(ManagerCodeEntity::getCodeTypeValue, type)
+				.eq(ManagerCodeEntity::getDataScope, DataSourceScope.COMMON_SCOPE.getValue()) ;
+
+		return list(query) ;
 	}
  
 }
