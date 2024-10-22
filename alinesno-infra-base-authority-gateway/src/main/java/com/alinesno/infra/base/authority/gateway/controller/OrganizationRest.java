ea90a5c4e8216ae47626b049e2e30fbc4a04dc9e
王磊(Levi) <leviAip@gmail.com>
2024-10-22 23:12:34 +0800

    feat:在 OrganizationRest 控制器中添加创建组织的功能

--- alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/controller/OrganizationRest.java
+++ alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/controller/OrganizationRest.java
@@ -1,12 +1,16 @@
 package com.alinesno.infra.base.authority.gateway.controller;
 
 import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.base.authority.annotation.DataPermissionScope;
+import com.alinesno.infra.base.authority.annotation.PermissionQuery;
+import com.alinesno.infra.base.authority.api.OrganizationDto;
 import com.alinesno.infra.base.authority.entity.OrganizationEntity;
 import com.alinesno.infra.base.authority.service.IOrganizationService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -35,13 +39,30 @@ public class OrganizationRest extends BaseController<OrganizationEntity, IOrgani
 	private IOrganizationService orgService;
 
 	@ApiOperation("分页查询")
-	
+
+	@DataPermissionScope
 	@PostMapping("/datatables")
 	public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
 		log.debug("page = {}", ToStringBuilder.reflectionToString(page));
 		// return this.toPage(model, this.getFeign(), page);
 
-        return orgService.queryOrganization(page);
+        return orgService.queryOrganization(page , CurrentAccountJwt.getUserId());
+	}
+
+	/**
+	 * 创建组织(createOrg)
+	 * @param dto
+	 * @return
+	 * @throws Exception
+	 */
+	@PostMapping("/createOrg")
+	public AjaxResult createOrg(@RequestBody OrganizationDto dto) {
+
+		dto.setOrganizationId(IdUtil.getSnowflakeNextIdStr());
+		dto.setDoorplateNumber(IdUtil.nanoId(8));
+
+		orgService.createOrJoinOrg(dto , CurrentAccountJwt.getUserId());
+		return ok();
 	}
 
 	@ResponseBody
