5548b1852464bbf5420096d3c333a629849b1f11
王磊(Levi) <leviAip@gmail.com>
2024-10-22 23:12:34 +0800

    feat:在 ManagerPositionRest 控制器中添加数据权限注解及日志记录功能

--- alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/controller/ManagerPositionRest.java
+++ alinesno-infra-base-authority-gateway/src/main/java/com/alinesno/infra/base/authority/gateway/controller/ManagerPositionRest.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.base.authority.gateway.controller;
 
+import com.alinesno.infra.base.authority.annotation.DataPermissionScope;
 import com.alinesno.infra.base.authority.entity.ManagerPositionEntity;
 import com.alinesno.infra.base.authority.service.IManagerPositionService;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
@@ -8,6 +9,7 @@ import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,19 +27,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author luoxiaodong 
  * @version 1.0.0
  */
+@Slf4j
 @Api(tags = "岗位管理")
 @RestController
 @Scope(SpringInstanceScope.PROTOTYPE)
 @RequestMapping("/api/infra/base/authority/managerPosition")
 public class ManagerPositionRest extends BaseController<ManagerPositionEntity, IManagerPositionService> {
 
-	// 日志记录
-	private static final Logger log = LoggerFactory.getLogger(ManagerPositionRest.class);
-
 	@Autowired
 	private IManagerPositionService managerPositionService;
 
-	
+
+	@DataPermissionScope
 	@ResponseBody
 	@PostMapping("/datatables")
 	public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
