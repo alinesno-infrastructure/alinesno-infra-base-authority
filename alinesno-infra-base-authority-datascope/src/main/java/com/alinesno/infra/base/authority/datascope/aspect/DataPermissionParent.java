74bc77c1bd813197b4d7a4f3499ef28ec867eac8
张婷(Tina) <tinaAip@gmail.com>
2024-10-23 19:23:02 +0800

    feat:新增数据权限拦截接口 `DataPermissionParent` 及其实现方法 `filterMap`。

---
+++ alinesno-infra-base-authority-datascope/src/main/java/com/alinesno/infra/base/authority/datascope/aspect/DataPermissionParent.java
@@ -0,0 +1,22 @@
+package com.alinesno.infra.base.authority.datascope.aspect;
+
+import com.alinesno.infra.common.facade.pageable.ConditionDto;
+import com.alinesno.infra.common.web.adapter.base.dto.ManagerAccountDto;
+
+/**
+ * 自定义数据权限拦截
+ * 
+ * @author LuoAnDong
+ * @since 2020年3月22日 下午5:56:47
+ */
+public interface DataPermissionParent {
+
+	/**
+	 * 前端参数
+	 * 
+	 * @param dto 参数
+	 * @param account  当前登录用户
+	 */
+	void filterMap(ConditionDto dto , ManagerAccountDto account);
+
+}
