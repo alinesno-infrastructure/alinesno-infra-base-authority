054a57cb62ee427f5d6339b79650318154316e56
张婷(Tina) <tinaAip@gmail.com>
2024-10-23 19:23:03 +0800

    feat:在 `ManagerProjectAccountServiceImpl` 中添加设置和获取当前项目的方法。

--- alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerProjectAccountServiceImpl.java
+++ alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/ManagerProjectAccountServiceImpl.java
@@ -1,11 +1,17 @@
 package com.alinesno.infra.base.authority.service.impl;
 
 import com.alinesno.infra.base.authority.entity.ManagerProjectAccountEntity;
+import com.alinesno.infra.base.authority.entity.ManagerProjectEntity;
 import com.alinesno.infra.base.authority.mapper.ManagerProjectAccountMapper;
+import com.alinesno.infra.base.authority.mapper.ManagerProjectMapper;
 import com.alinesno.infra.base.authority.service.IManagerProjectAccountService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,11 +23,38 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service
-public class ManagerProjectAccountServiceImpl
-		extends IBaseServiceImpl<ManagerProjectAccountEntity, ManagerProjectAccountMapper>
-		implements IManagerProjectAccountService {
+public class ManagerProjectAccountServiceImpl  extends IBaseServiceImpl<ManagerProjectAccountEntity, ManagerProjectAccountMapper>  implements IManagerProjectAccountService {
 
+	@Autowired
+	private ManagerProjectMapper managerProjectMapper ;
 
+	@Override
+	public void setCurrentProject(long projectId, long userId) {
 
+		// 查询当前用户配置记录
+		long count = count(new LambdaQueryWrapper<ManagerProjectAccountEntity>()
+				.eq(ManagerProjectAccountEntity::getAccountId , userId)) ;
 
+		ManagerProjectAccountEntity e = new ManagerProjectAccountEntity() ;
+
+		e.setAccountId(userId);
+		e.setApplicationId(projectId);
+		e.setAppOrder(count+1);
+
+		save(e);
+	}
+
+	@Override
+	public ManagerProjectEntity getCurrentProject(long userId) {
+
+		List<ManagerProjectAccountEntity> es = list(new LambdaQueryWrapper<ManagerProjectAccountEntity>()
+				.eq(ManagerProjectAccountEntity::getAccountId , userId)
+				.orderByDesc(ManagerProjectAccountEntity::getAppOrder));
+
+		if(es != null && !es.isEmpty()){
+			return managerProjectMapper.selectById(es.get(0).getApplicationId()) ;
+		}
+
+		return null;
+	}
 }
