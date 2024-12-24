1a6a85117894c17152a56ffdddab50a537479f2d
陈伟(Victor) <victorAip@gmail.com>
2024-12-25 00:10:56 +0800

    fix: 注释专家导航项以避免功能失效

--- alinesno-infra-base-authority-ui/src/layout/components/TopHeader/nav.vue
+++ alinesno-infra-base-authority-ui/src/layout/components/TopHeader/nav.vue
@@ -10,11 +10,11 @@
         <i class="fa-regular fa-file-word"></i> 手册
       </router-link>
     </div>
-    <div class="acp-header-item ">
+    <!-- <div class="acp-header-item ">
       <router-link class="header-label-text" to="/dashboard/smartService">
         <i class="fa-solid fa-swatchbook"></i> 专家 
       </router-link>
-    </div>
+    </div> -->
     <div class="acp-header-item ">
       <router-link class="header-label-text" to="/dashboard/suportTechnique">
         <i class="fa-solid fa-user-tag"></i> 服务
