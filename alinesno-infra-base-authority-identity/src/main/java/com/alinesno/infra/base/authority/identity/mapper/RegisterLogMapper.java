a7b1c70271239fc64c4a7159cf9ff45906f949ff
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:08 +0800

    feat:新增 RegisterLogMapper 接口，继承 IBaseMapper 定义注册日志数据操作。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/mapper/RegisterLogMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.authority.identity.mapper;
+
+import com.alinesno.infra.base.authority.identity.domain.RegisterLogEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface RegisterLogMapper extends IBaseMapper<RegisterLogEntity> {
+}    
