6757d4258064d1507d85079d3525e99b113da994
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:07 +0800

    feat:新增 AuthManagerAccountDto 类，封装 manager_account 表的 DTO 对象，并提供 DTO 和 Entity 的转换方法。

---
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/AuthManagerAccountDto.java
@@ -0,0 +1,139 @@
+package com.alinesno.infra.base.authority.gateway.dto;
+
+import com.alinesno.infra.base.authority.api.OrganizationDto;
+import com.alinesno.infra.base.authority.entity.ManagerAccountEntity;
+import com.alinesno.infra.common.facade.base.BaseDto;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 功能名： 【请填写功能名称】 数据表： manager_account
+ * 
+ * @author luoxiaodong 
+ * @version 1.0.0
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AuthManagerAccountDto extends BaseDto {
+	// fields
+	/**
+	 * "账户状态"
+	 */
+	private String accountStatus;
+	/**
+	 * "最后登陆IP"
+	 */
+	private String lastLoginIp;
+	/**
+	 * "lastLoginTime"
+	 */
+	private String lastLoginTime;
+	/**
+	 * "loginName"
+	 */
+	private String loginName;
+	/**
+	 * "name"
+	 */
+	private String name;
+	/**
+	 * "所属者"
+	 */
+	private String owners;
+	/**
+	 * "password"
+	 */
+	private String password;
+	/**
+	 * "roleId"
+	 */
+	private String roleId;
+	/**
+	 * "rolePower"
+	 */
+	private String rolePower;
+	/**
+	 * "salt"
+	 */
+	private String salt;
+	/**
+	 * "userId"
+	 */
+	private String userId;
+	/**
+	 * "department"
+	 */
+	private String department;
+	/**
+	 * "email"
+	 */
+	private String email;
+	/**
+	 * "phone"
+	 */
+	private String phone;
+	/**
+	 * "sex"
+	 */
+	private String sex;
+	/**
+	 * "hasEditor"
+	 */
+	private String hasEditor;
+	/**
+	 * "用户头像"
+	 */
+	private String avatarPath;
+	/**
+	 * "企业属性"
+	 */
+	private String enterpriseProperties;
+	/**
+	 * "portalProp"
+	 */
+	private String portalProp;
+	/**
+	 * "岗位id"
+	 */
+	private String positionId;
+
+	// 默认授权信息
+	private List<String> authRole;
+
+	/**
+	 * 组织岗位类型
+	 */
+	private Integer orgType ;
+
+	/**
+	 * 组织列表
+	 */
+	private List<OrganizationDto> orgList ;
+
+	/**
+	 * 快速从 dto 转换成 entity
+	 * 
+	 * @return dto 对应的 entity
+	 */
+	public AuthManagerAccountDto mapToDto() {
+		ObjectMapper toDtoMapper = new ObjectMapper();
+		toDtoMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return toDtoMapper.convertValue(this, AuthManagerAccountDto.class);
+	}
+
+	/**
+	 * 快速从 entity 转换成 dto
+	 * 
+	 * @return dto 对应的 entity
+	 */
+	public AuthManagerAccountDto fromDto(ManagerAccountEntity entity) {
+		ObjectMapper fromDtoMapper = new ObjectMapper();
+		fromDtoMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return fromDtoMapper.convertValue(entity, AuthManagerAccountDto.class);
+	}
+
+}
