ba8e3dafbd488f5a05562cd9103ccc2e9a249241
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:09 +0800

    feat:新增 EnterpriseAccountDto 类，引入 Lombok 注解优化代码结构。

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/EnterpriseAccountDto.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/EnterpriseAccountDto.java
@@ -1,6 +1,8 @@
 package com.alinesno.infra.base.authority.gateway.dto;
 
 import com.alinesno.infra.common.facade.base.BaseDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 企业信息账户管理
@@ -8,34 +10,12 @@ import com.alinesno.infra.common.facade.base.BaseDto;
  * @author luoxiaodong
  * @since 2023年10月27日 下午3:05:15
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class EnterpriseAccountDto extends BaseDto {
 
 	private String enterpriseId; // 企业id
 	private String creditCode; // 企业信用代码
 	private String accountId; // 账号id
-
-	public String getCreditCode() {
-		return creditCode;
-	}
-
-	public void setCreditCode(String creditCode) {
-		this.creditCode = creditCode;
-	}
-
-	public String getEnterpriseId() {
-		return enterpriseId;
-	}
-
-	public void setEnterpriseId(String enterpriseId) {
-		this.enterpriseId = enterpriseId;
-	}
-
-	public String getAccountId() {
-		return accountId;
-	}
-
-	public void setAccountId(String accountId) {
-		this.accountId = accountId;
-	}
 
 }
