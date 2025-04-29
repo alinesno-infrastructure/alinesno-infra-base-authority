9ee8fa4204ac3b77a4c8cfcb63d2be39b4454c25
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:09 +0800

    refactor: 优化 AuthorizeUsersParams 类，使用 Lombok 注解减少冗余 getter 和 setter 方法

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/param/AuthorizeUsersParams.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/param/AuthorizeUsersParams.java
@@ -1,28 +1,15 @@
 package com.alinesno.infra.base.authority.gateway.param;
 
 import com.alinesno.infra.base.authority.entity.ManagerRoleEntity;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.List;
 
-@SuppressWarnings("serial")
+@Data
 public class AuthorizeUsersParams implements Serializable {
+
 	private List<ManagerRoleEntity> roles;
 	private String accountId;
 
-	public List<ManagerRoleEntity> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<ManagerRoleEntity> roles) {
-		this.roles = roles;
-	}
-
-	public String getAccountId() {
-		return accountId;
-	}
-
-	public void setAccountId(String accountId) {
-		this.accountId = accountId;
-	}
 }
