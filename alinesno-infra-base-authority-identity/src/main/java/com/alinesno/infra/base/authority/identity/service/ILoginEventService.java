7af168a9d64c569eba8ebe620720559d718613cb
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:05 +0800

    feat:新增 ILoginEventService 接口，定义登录事件服务基础操作。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/service/ILoginEventService.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.base.authority.identity.service;
+
+import com.alinesno.infra.base.authority.identity.domain.LoginEventEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author WeiXiaoJin
+ * @version 1.0.0
+ */
+
+public interface ILoginEventService extends IBaseService<LoginEventEntity> {
+
+}
