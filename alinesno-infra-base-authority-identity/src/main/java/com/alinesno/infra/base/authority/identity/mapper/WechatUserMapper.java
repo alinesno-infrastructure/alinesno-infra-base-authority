9ccfa221ff05c20dd633046b716d7a23f8fe87db
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:06 +0800

    feat:新增 WechatUserMapper 接口，用于微信用户实体的持久层操作。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/mapper/WechatUserMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.authority.identity.mapper;
+
+import com.alinesno.infra.base.authority.identity.domain.WechatUserEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WechatUserMapper extends IBaseMapper<WechatUserEntity> {
+
+}
