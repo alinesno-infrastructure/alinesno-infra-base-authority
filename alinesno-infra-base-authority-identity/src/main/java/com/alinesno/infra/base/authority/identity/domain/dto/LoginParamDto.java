04c01155e02b1861c009fcf9902b423c9c13e9da
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:07 +0800

    feat:新增 LoginParamDto 类，包含用户名、密码、验证码及唯一标识等字段。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/domain/dto/LoginParamDto.java
@@ -0,0 +1,27 @@
+package com.alinesno.infra.base.authority.identity.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginParamDto {
+	/**
+	 * 用户名
+	 */
+	private String username;
+
+	/**
+	 * 用户密码
+	 */
+	private String password;
+
+	/**
+	 * 验证码
+	 */
+	private String code;
+
+	/**
+	 * 唯一标识
+	 */
+	private String uuid = "";
+
+}
