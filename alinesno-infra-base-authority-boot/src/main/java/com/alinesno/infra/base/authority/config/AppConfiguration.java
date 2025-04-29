0489dda5594e852bcb3182b1764ef40a9b441c07
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:09 +0800

    refactor: 移除 AppConfiguration 中的初始化逻辑并调整 MapperScan 包路径

--- alinesno-infra-base-authority-boot/src/main/java/com/alinesno/infra/base/authority/config/AppConfiguration.java
+++ alinesno-infra-base-authority-boot/src/main/java/com/alinesno/infra/base/authority/config/AppConfiguration.java
@@ -1,13 +1,11 @@
 package com.alinesno.infra.base.authority.config;
 
-import com.alinesno.infra.base.authority.initialize.IAuthorityInitService;
 import com.alinesno.infra.common.facade.enable.EnableActable;
+import com.alinesno.infra.common.security.api.enable.EnableInfraApiSecurity;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
 import com.dtflys.forest.springboot.annotation.ForestScan;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,40 +20,15 @@ import org.springframework.context.annotation.Configuration;
 })
 @MapperScan({
     "com.alinesno.infra.base.authority.mapper" ,
-    "com.alinesno.infra.base.authority.meta.mapper"
+    "com.alinesno.infra.base.authority.identity.mapper"
 })
 @EnableInfraAuthorityApi
 @Configuration
-public class AppConfiguration implements CommandLineRunner {
-
-    @Autowired
-    private IAuthorityInitService  authorityInitService;
+public class AppConfiguration {
 
     @Bean
     public LogAspect getLogAspect() {
         return new LogAspect();
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.debug("项目启动初始化");
-
-//        // 初始化平台默认组织
-//        OrganizationEntity org = authorityInitService.initOrganization() ;
-//
-//        // 初始化平台默认参数和字典
-//        authorityInitService.initManagerCode(org) ;
-//        authorityInitService.initManagerConfig(org) ;
-//
-//        // 初始化超级管理员
-//        ManagerAccountEntity managerAccount = authorityInitService.initSuperManager(org) ;
-//
-//        // 初始化权限引擎服务的菜单权限
-//        ManagerProjectEntity project = authorityInitService.initData(managerAccount.getId() , org) ;
-//
-//        // 初始化部门和岗位数据
-//        authorityInitService.initDeptAndPost(managerAccount.getId() , org , project) ;
-
     }
 
 }
