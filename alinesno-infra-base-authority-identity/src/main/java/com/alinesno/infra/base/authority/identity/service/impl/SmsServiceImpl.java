d50457b3f3dddefd153039b41dcb03e2a89634d1
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:05 +0800

    feat: 实现 SmsServiceImpl，提供发送短信服务功能。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/service/impl/SmsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.base.authority.identity.service.impl;
+
+import com.alinesno.infra.base.authority.identity.enums.SmsProviderEnums;
+import com.alinesno.infra.base.authority.identity.service.SmsService;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.sms4j.api.SmsBlend;
+import org.dromara.sms4j.api.entity.SmsResponse;
+import org.dromara.sms4j.core.factory.SmsFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+/**
+ * 发送短信服务
+ */
+@Slf4j
+@Component
+public class SmsServiceImpl implements SmsService {
+
+    @Override
+    public SmsResponse sendMessage(String phone, String message, String configId) {
+
+        if(!StringUtils.hasLength(configId)){
+            configId = SmsProviderEnums.ALIYUN.getCode() ;
+        }
+
+        SmsBlend smsBlend = SmsFactory.getSmsBlend(configId);
+        Assert.notNull(smsBlend, "短信服务配置[" + configId + "]不存在");
+
+        SmsResponse smsResponse = smsBlend.sendMessage(phone, message);
+
+        log.debug("smsResponse = {}", smsResponse.getData());
+
+        return smsResponse ;
+    }
+
+}
