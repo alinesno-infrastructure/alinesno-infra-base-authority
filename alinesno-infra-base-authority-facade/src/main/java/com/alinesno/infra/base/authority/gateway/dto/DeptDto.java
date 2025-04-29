ec4d5e843766352f16d488b79d573b3fbc69be6f
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:09 +0800

    refactor: 优化 DeptDto 类，使用 Lombok 注解替换冗长的 getter 和 setter 方法。

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/DeptDto.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/DeptDto.java
@@ -1,39 +1,18 @@
 package com.alinesno.infra.base.authority.gateway.dto;
 
+import lombok.Data;
+
 /**
  * 机构机构
- * 
+ *
  * @author luoxiaodong
  * @since 2023年8月23日 下午8:10:43
  */
+@Data
 public class DeptDto {
 
-	private Long deptId;  
-	private String deptName;  
-	private Long parentDeptId;  
-
-	public Long getDeptId() {
-		return deptId;
-	}
-
-	public void setDeptId(Long deptId) {
-		this.deptId = deptId;
-	}
-
-	public String getDeptName() {
-		return deptName;
-	}
-
-	public void setDeptName(String deptName) {
-		this.deptName = deptName;
-	}
-
-	public Long getParentDeptId() {
-		return parentDeptId;
-	}
-
-	public void setParentDeptId(Long parentDeptId) {
-		this.parentDeptId = parentDeptId;
-	}
+	private Long deptId;
+	private String deptName;
+	private Long parentDeptId;
 
 }
