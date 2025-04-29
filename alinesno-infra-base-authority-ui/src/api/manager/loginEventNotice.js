bcadfa7053561e8d401de6197fcf84f5eca86e1c
梁明树(Nina) <ninaAip@gmail.com>
2025-04-29 22:54:07 +0800

    feat:新增登录事件通知相关接口封装，包含查询、新增、修改、删除等功能。

---
+++ alinesno-infra-base-authority-ui/src/api/manager/loginEventNotice.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+import {parseStrEmpty} from "@/utils/ruoyi";
+
+// 接口配置项
+var prefix = '/api/infra/base/authority/notices/loginEvent/';
+var managerUrl = {
+    datatables: prefix + "datatables",
+    createUrl: prefix + 'add',
+    saveUrl: prefix + 'save',
+    updateUrl: prefix + "modify",
+    statusUrl: prefix + "changeStatus",
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix + "detail",
+    removeUrl: prefix + "delete",
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    downloadfile: prefix + "downloadfile"
+}
+
+// 查询公告列表
+export function listNotice(query) {
+    return request({
+        url: managerUrl.datatables,
+        method: 'post',
+        params: query
+    })
+}
+
+// 查询公告详细
+export function getNotice(id) {
+    return request({
+        url: managerUrl.detailUrl + '/' + parseStrEmpty(id),
+        method: 'get'
+    })
+}
+
+// 新增公告
+export function addNotice(data) {
+    return request({
+        url: managerUrl.saveUrl,
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改公告
+export function updateNotice(data) {
+    return request({
+        url: managerUrl.updateUrl,
+        method: 'put',
+        data: data
+    })
+}
+
+// 删除公告
+export function delNotice(id) {
+    return request({
+        url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+        method: 'delete'
+    })
+}
