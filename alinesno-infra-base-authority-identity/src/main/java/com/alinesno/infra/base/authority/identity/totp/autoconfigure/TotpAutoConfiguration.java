30aa8c701d8f0132cffc8bc9f82fbb840eae5712
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:06 +0800

    feat: 添加 TotpAutoConfiguration 配置类及依赖组件的默认实现

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/totp/autoconfigure/TotpAutoConfiguration.java
@@ -0,0 +1,92 @@
+package com.alinesno.infra.base.authority.identity.totp.autoconfigure;
+
+import com.alinesno.infra.base.authority.identity.totp.TotpInfo;
+import com.alinesno.infra.base.authority.identity.totp.code.*;
+import com.alinesno.infra.base.authority.identity.totp.qr.QrDataFactory;
+import com.alinesno.infra.base.authority.identity.totp.qr.QrGenerator;
+import com.alinesno.infra.base.authority.identity.totp.qr.ZxingPngQrGenerator;
+import com.alinesno.infra.base.authority.identity.totp.recovery.RecoveryCodeGenerator;
+import com.alinesno.infra.base.authority.identity.totp.secret.DefaultSecretGenerator;
+import com.alinesno.infra.base.authority.identity.totp.secret.SecretGenerator;
+import com.alinesno.infra.base.authority.identity.totp.time.SystemTimeProvider;
+import com.alinesno.infra.base.authority.identity.totp.time.TimeProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(TotpInfo.class)
+@EnableConfigurationProperties(TotpProperties.class)
+public class TotpAutoConfiguration {
+
+    private TotpProperties props;
+
+    @Autowired
+    public TotpAutoConfiguration(TotpProperties props) {
+        this.props = props;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SecretGenerator secretGenerator() {
+        int length = props.getSecret().getLength();
+        return new DefaultSecretGenerator(length);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TimeProvider timeProvider() {
+        return new SystemTimeProvider();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public HashingAlgorithm hashingAlgorithm() {
+        return HashingAlgorithm.SHA1;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public QrDataFactory qrDataFactory(HashingAlgorithm hashingAlgorithm) {
+        return new QrDataFactory(hashingAlgorithm, getCodeLength(), getTimePeriod());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public QrGenerator qrGenerator() {
+        return new ZxingPngQrGenerator();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CodeGenerator codeGenerator(HashingAlgorithm algorithm) {
+        return new DefaultCodeGenerator(algorithm, getCodeLength());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CodeVerifier codeVerifier(CodeGenerator codeGenerator, TimeProvider timeProvider) {
+        DefaultCodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+        verifier.setTimePeriod(getTimePeriod());
+        verifier.setAllowedTimePeriodDiscrepancy(props.getTime().getDiscrepancy());
+
+        return verifier;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RecoveryCodeGenerator recoveryCodeGenerator() {
+        return new RecoveryCodeGenerator();
+    }
+
+    private int getCodeLength() {
+        return props.getCode().getLength();
+    }
+
+    private int getTimePeriod() {
+        return props.getTime().getPeriod();
+    }
+}
