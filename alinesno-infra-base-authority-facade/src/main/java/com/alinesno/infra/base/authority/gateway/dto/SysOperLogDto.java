e90e02a76e800a79cfa3eaecc4cccef9e40183f7
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:09 +0800

    refactor: 使用 Lombok 注解优化 SysOperLogDto 类的代码结构

--- alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/SysOperLogDto.java
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/gateway/dto/SysOperLogDto.java
@@ -3,6 +3,8 @@ package com.alinesno.infra.base.authority.gateway.dto;
 import com.alinesno.infra.common.facade.base.BaseDto;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 功能名： 【请填写功能名称】 数据表： sys_oper_log
@@ -10,8 +12,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author luoxiaodong 
  * @version 1.0.0
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class SysOperLogDto extends BaseDto {
-	private static final long serialVersionUID = 1L;
 	// fields
 	/**
 	 * "agent"
@@ -73,172 +76,6 @@ public class SysOperLogDto extends BaseDto {
 	 * "url"
 	 */
 	private String url;
-
-	// getter and setter
-	public String getAgent()
-
-	{
-		return this.agent;
-	}
-
-	public SysOperLogDto setAgent(String arg) {
-		this.agent = arg;
-		return this;
-	}
-
-	public String getContractNumber()
-
-	{
-		return this.contractNumber;
-	}
-
-	public SysOperLogDto setContractNumber(String arg) {
-		this.contractNumber = arg;
-		return this;
-	}
-
-	public String getDataId()
-
-	{
-		return this.dataId;
-	}
-
-	public SysOperLogDto setDataId(String arg) {
-		this.dataId = arg;
-		return this;
-	}
-
-	public String getFlowTaskId()
-
-	{
-		return this.flowTaskId;
-	}
-
-	public SysOperLogDto setFlowTaskId(String arg) {
-		this.flowTaskId = arg;
-		return this;
-	}
-
-	public String getIp()
-
-	{
-		return this.ip;
-	}
-
-	public SysOperLogDto setIp(String arg) {
-		this.ip = arg;
-		return this;
-	}
-
-	public String getMethodDesc()
-
-	{
-		return this.methodDesc;
-	}
-
-	public SysOperLogDto setMethodDesc(String arg) {
-		this.methodDesc = arg;
-		return this;
-	}
-
-	public String getOperContent()
-
-	{
-		return this.operContent;
-	}
-
-	public SysOperLogDto setOperContent(String arg) {
-		this.operContent = arg;
-		return this;
-	}
-
-	public String getOperOpinion()
-
-	{
-		return this.operOpinion;
-	}
-
-	public SysOperLogDto setOperOpinion(String arg) {
-		this.operOpinion = arg;
-		return this;
-	}
-
-	public String getOperStatus()
-
-	{
-		return this.operStatus;
-	}
-
-	public SysOperLogDto setOperStatus(String arg) {
-		this.operStatus = arg;
-		return this;
-	}
-
-	public Long getOperTime()
-
-	{
-		return this.operTime;
-	}
-
-	public SysOperLogDto setOperTime(Long arg) {
-		this.operTime = arg;
-		return this;
-	}
-
-	public String getOperUserId()
-
-	{
-		return this.operUserId;
-	}
-
-	public SysOperLogDto setOperUserId(String arg) {
-		this.operUserId = arg;
-		return this;
-	}
-
-	public String getOperUserName()
-
-	{
-		return this.operUserName;
-	}
-
-	public SysOperLogDto setOperUserName(String arg) {
-		this.operUserName = arg;
-		return this;
-	}
-
-	public String getParams()
-
-	{
-		return this.params;
-	}
-
-	public SysOperLogDto setParams(String arg) {
-		this.params = arg;
-		return this;
-	}
-
-	public String getRecordType()
-
-	{
-		return this.recordType;
-	}
-
-	public SysOperLogDto setRecordType(String arg) {
-		this.recordType = arg;
-		return this;
-	}
-
-	public String getUrl()
-
-	{
-		return this.url;
-	}
-
-	public SysOperLogDto setUrl(String arg) {
-		this.url = arg;
-		return this;
-	}
 
 	/**
 	 * 快速从 dto 转换成 entity
