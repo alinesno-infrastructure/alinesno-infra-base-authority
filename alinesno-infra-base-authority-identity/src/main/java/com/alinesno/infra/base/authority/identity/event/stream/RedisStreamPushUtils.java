2d5c42f279bc9b18689b9560201a4022c6cdb215
黄涛(Leo) <leoAip@gmail.com>
2025-04-29 22:54:07 +0800

    feat:新增 RedisStreamPushUtils 工具类，支持向Redis Stream推送消息功能。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/event/stream/RedisStreamPushUtils.java
@@ -0,0 +1,27 @@
+package com.alinesno.infra.base.authority.identity.event.stream;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.Record;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RedisStreamPushUtils {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 推送消息内容
+     * @param msgContext
+     * @param topic
+     */
+    public void pushTask(Object msgContext , String topic){
+        stringRedisTemplate.opsForStream().add(Record.of(JSONObject.toJSONString(msgContext)).withStreamKey(topic));
+        log.info("{}已发送消息:{}", topic,msgContext);
+    }
+
+}
