a01e10b3b869da5256c111fccbf6a3677ea5a65a
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:53:59 +0800

    更新:alinesno-infra-base-identity-auth-mfa/src/main/java/com/alinesno/infra/base/identity/auth/totp/secret/SecretGenerator.java

--- alinesno-infra-base-identity-auth-mfa/src/main/java/com/alinesno/infra/base/identity/auth/totp/secret/SecretGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.alinesno.infra.base.identity.auth.totp.secret;
-
-public interface SecretGenerator {
-    /**
-     * @return A random base32 encoded string to use as the shared secret/key between the server and the client.
-     */
-    String generate();
-}
