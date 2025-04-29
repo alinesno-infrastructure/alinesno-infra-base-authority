671d43a056c182c51659590c791b0b8f53a864ac
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:06 +0800

    feat:新增 IEnterpriseThemeService 接口，定义企业主题服务的基础操作。

---
+++ alinesno-infra-base-authority-facade/src/main/java/com/alinesno/infra/base/authority/service/IEnterpriseThemeService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.authority.service;
+
+import com.alinesno.infra.base.authority.entity.EnterpriseThemeEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+public interface IEnterpriseThemeService extends IBaseService<EnterpriseThemeEntity> {
+}
