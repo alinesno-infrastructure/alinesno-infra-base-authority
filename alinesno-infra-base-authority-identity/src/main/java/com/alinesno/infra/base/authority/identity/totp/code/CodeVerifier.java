b20dc9c840d62fb24d9584bfc1aa30f6c3454203
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:05 +0800

    feat:新增 CodeVerifier 接口，用于验证 TOTP 动态验证码的有效性。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/totp/code/CodeVerifier.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.base.authority.identity.totp.code;
+
+public interface CodeVerifier {
+    /**
+     * @param secret The shared secret/key to check the code against.
+     * @param code The n-digit code given by the end user to check.
+     * @return If the code is valid or not.
+     */
+    boolean isValidCode(String secret, String code);
+}
