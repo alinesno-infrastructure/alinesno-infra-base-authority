8405514fa41cceaa1489f47dc7dd1f203c392db7
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:05 +0800

    feat:新增 LoginEventMapper 接口，定义登录事件持久层操作。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/mapper/LoginEventMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.authority.identity.mapper;
+
+import com.alinesno.infra.base.authority.identity.domain.LoginEventEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface LoginEventMapper extends IBaseMapper<LoginEventEntity> {
+
+}
