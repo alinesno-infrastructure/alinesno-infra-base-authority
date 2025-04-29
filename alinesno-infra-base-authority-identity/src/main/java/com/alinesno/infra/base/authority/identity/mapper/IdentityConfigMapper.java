fc0412e691b18de4643f7e0dbb1fb0eea105093e
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:08 +0800

    feat:新增 IdentityConfigMapper 接口，定义持久层操作方法。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/mapper/IdentityConfigMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.authority.identity.mapper;
+
+import com.alinesno.infra.base.authority.identity.domain.IdentityConfigEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IdentityConfigMapper extends IBaseMapper<IdentityConfigEntity> {
+
+}
