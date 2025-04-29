75f84b4fd91d1ca50d7bdf7614ca86c7eca2478f
王明(AllenQin) <allenQin@gmail.com>
2025-04-29 22:54:06 +0800

    feat:新增 header.vue 组件，用于展示系统头部信息并支持跳转官网。

---
+++ alinesno-infra-base-identity-ui/src/views/common/header.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <el-header style="z-index: 100;height: 45px;background: #fff;border-bottom: 0px solid #e5e5e5;">
+      <div>
+        <div style="float: left;font-size: 30px;color:#fff;margin-top: 5px;">
+          <img :src="identityInfoPanel.logo" style="width:35px" alt="">
+        </div>
+        <div class="banner-text">
+          {{ identityInfoPanel.systemName }}
+        </div>
+        <a :href="identityInfoPanel.portalSite"
+           target="_blank"
+           style="float: right;font-weight: 500;font-size: 13px;margin-top: 14px;color:#222;">
+          <i class="fas fa-link"></i> 官网
+        </a>
+      </div>
+    </el-header>
+
+</template>
+
+<script setup>
+
+const props = defineProps({
+  identityInfoPanel: {
+    type: Object,
+    default: () => {
+      return {
+        logo: '',
+        systemName: '',
+        portalSite: ''
+      }
+    }
+  }
+})
+
+</script>
+
+<style scoped lang="scss">
+</style>
+
+
