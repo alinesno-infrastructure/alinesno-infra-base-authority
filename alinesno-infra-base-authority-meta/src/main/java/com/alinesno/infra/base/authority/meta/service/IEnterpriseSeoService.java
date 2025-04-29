5932d79133383ea00f4fb2a4c176b0691d4ae909
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:04 +0800

    更新:alinesno-infra-base-authority-meta/src/main/java/com/alinesno/infra/base/authority/meta/service/IEnterpriseSeoService.java

--- alinesno-infra-base-authority-meta/src/main/java/com/alinesno/infra/base/authority/meta/service/IEnterpriseSeoService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.base.authority.meta.service;
-
-import com.alinesno.infra.base.authority.meta.entity.EnterpriseSeoEntity;
-import com.alinesno.infra.common.facade.services.IBaseService;
-
-public interface IEnterpriseSeoService extends IBaseService<EnterpriseSeoEntity> {
-}
