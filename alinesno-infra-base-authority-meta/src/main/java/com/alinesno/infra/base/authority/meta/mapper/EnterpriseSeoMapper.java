c11e4582599e3d70b62ad02e8ed429d6c3c3151c
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:01 +0800

    更新:alinesno-infra-base-authority-meta/src/main/java/com/alinesno/infra/base/authority/meta/mapper/EnterpriseSeoMapper.java

--- alinesno-infra-base-authority-meta/src/main/java/com/alinesno/infra/base/authority/meta/mapper/EnterpriseSeoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.base.authority.meta.mapper;
-
-import com.alinesno.infra.base.authority.meta.entity.EnterpriseSeoEntity;
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-
-public interface EnterpriseSeoMapper extends IBaseMapper<EnterpriseSeoEntity> {
-}
