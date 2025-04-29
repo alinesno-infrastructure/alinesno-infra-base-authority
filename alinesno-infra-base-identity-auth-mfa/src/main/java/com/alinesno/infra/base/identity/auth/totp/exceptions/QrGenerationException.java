72abb96ca4d5aa552a2981d4ab052aa208904cae
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:53:59 +0800

    更新:alinesno-infra-base-identity-auth-mfa/src/main/java/com/alinesno/infra/base/identity/auth/totp/exceptions/QrGenerationException.java

--- alinesno-infra-base-identity-auth-mfa/src/main/java/com/alinesno/infra/base/identity/auth/totp/exceptions/QrGenerationException.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.base.identity.auth.totp.exceptions;
-
-public class QrGenerationException extends Exception {
-    public QrGenerationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
