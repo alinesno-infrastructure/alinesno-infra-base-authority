146f23f618b5477200838c44f0d9f250d4d7648c
王明(AllenQin) <allenQin@gmail.com>
2025-04-29 22:54:10 +0800

    fix:修复 resetPwd.vue 表单组件 size 属性缺失问题，优化密码重置页面样式一致性。

--- alinesno-infra-base-authority-ui/src/views/system/user/profile/resetPwd.vue
+++ alinesno-infra-base-authority-ui/src/views/system/user/profile/resetPwd.vue
@@ -1,5 +1,5 @@
 <template>
-   <el-form ref="pwdRef" :model="user" :rules="rules" label-width="80px">
+   <el-form ref="pwdRef" :model="user" :rules="rules" label-width="80px" size="large">
       <el-form-item label="旧密码" prop="oldPassword">
          <el-input v-model="user.oldPassword" placeholder="请输入旧密码" type="password" show-password />
       </el-form-item>
