15382986c01e57b1acd0c60414a2dd005b1c5fc6
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:05 +0800

    feat:新增 DefaultSecretGenerator 类，实现 TOTP 密钥生成逻辑。

---
+++ alinesno-infra-base-authority-identity/src/main/java/com/alinesno/infra/base/authority/identity/totp/secret/DefaultSecretGenerator.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.base.authority.identity.totp.secret;
+
+import org.apache.commons.codec.binary.Base32;
+
+import java.security.SecureRandom;
+
+@SuppressWarnings("WeakerAccess")
+public class DefaultSecretGenerator implements SecretGenerator {
+
+    private final SecureRandom randomBytes = new SecureRandom();
+    private final static Base32 encoder = new Base32();
+    private final int numCharacters;
+
+    public DefaultSecretGenerator() {
+        this.numCharacters = 32;
+    }
+
+    /**
+     * @param numCharacters The number of characters the secret should consist of.
+     */
+    public DefaultSecretGenerator(int numCharacters) {
+        this.numCharacters = numCharacters;
+    }
+
+    @Override
+    public String generate() {
+        return new String(encoder.encode(getRandomBytes()));
+    }
+
+    private byte[] getRandomBytes() {
+        // 5 bits per char in base32
+        byte[] bytes = new byte[(numCharacters * 5) / 8];
+        randomBytes.nextBytes(bytes);
+
+        return bytes;
+    }
+}
