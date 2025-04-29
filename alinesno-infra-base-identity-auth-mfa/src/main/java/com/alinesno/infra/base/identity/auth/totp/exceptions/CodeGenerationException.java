3a82bc576cbec4de7bd4ed1e9ec5e82493723ba0
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:53:58 +0800

    更新:alinesno-infra-base-identity-auth-mfa/src/main/java/com/alinesno/infra/base/identity/auth/totp/exceptions/CodeGenerationException.java

--- alinesno-infra-base-identity-auth-mfa/src/main/java/com/alinesno/infra/base/identity/auth/totp/exceptions/CodeGenerationException.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.base.identity.auth.totp.exceptions;
-
-public class CodeGenerationException extends Exception {
-    public CodeGenerationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
