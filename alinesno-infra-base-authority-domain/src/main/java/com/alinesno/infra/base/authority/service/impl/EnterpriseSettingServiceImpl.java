30af928f76af3f146974e2de951ea3ffbf8258d3
李娜(Allen) <allenAip@gmail.com>
2025-04-29 22:54:09 +0800

    feat:新增 EnterpriseSettingServiceImpl 类，实现企业设置服务功能。

---
+++ alinesno-infra-base-authority-domain/src/main/java/com/alinesno/infra/base/authority/service/impl/EnterpriseSettingServiceImpl.java
@@ -0,0 +1,23 @@
+package com.alinesno.infra.base.authority.service.impl;
+
+import com.alinesno.infra.base.authority.entity.EnterpriseSettingEntity;
+import com.alinesno.infra.base.authority.mapper.EnterpriseSettingMapper;
+import com.alinesno.infra.base.authority.service.IEnterpriseSettingService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author yangboot
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class EnterpriseSettingServiceImpl extends IBaseServiceImpl<EnterpriseSettingEntity, EnterpriseSettingMapper> implements IEnterpriseSettingService {
+
+
+}
