18bdc6e387fb68ff20eefd9be410b8ac3207c812
王明(AllenQin) <allenQin@gmail.com>
2025-04-29 22:54:05 +0800

    feat:新增 Footer 组件，用于展示版权信息和系统标识。

---
+++ alinesno-infra-base-identity-ui/src/views/common/footer.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <el-footer style="background: rgb(250, 250, 250);bottom: 0px;width: 100%;height: 45px;">
+      <div class="copyright-text">© {{ identityInfoPanel.copyrightYear }} {{ identityInfoPanel.copyrightLabel }}</div>
+    </el-footer>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  identityInfoPanel: {
+    type: Object,
+    default: () => {
+      return {
+        logo: '',
+        systemName: '',
+        portalSite: ''
+      }
+    }
+  }
+})
+
+</script>
+
+<style scoped lang="scss">
+</style>
+
+
