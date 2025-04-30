19760e7a05075cd6cd4e7d4ecc38ba118a8d22af
李伟(Michael) <michaelAip@gmail.com>
2025-05-01 00:12:23 +0800

    fix:修复登录接口中 Authorization 头部格式问题，添加 Bearer 前缀。

--- alinesno-infra-base-identity-ui/src/api/login.js
+++ alinesno-infra-base-identity-ui/src/api/login.js
@@ -121,7 +121,7 @@ export function getRedirectUrl(pData){
     url: '/sso/getRedirectUrl',
     headers: {
       'X-Requested-With': 'XMLHttpRequest',
-	  'Authorization': localStorage.getItem('satoken')
+	  'Authorization': 'Bearer ' + localStorage.getItem('satoken')
     },
     method: 'post',
     data: pData 
