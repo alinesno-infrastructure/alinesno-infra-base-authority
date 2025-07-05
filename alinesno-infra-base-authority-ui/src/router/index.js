9aeb9605e7bfefdad483eba6de6808563088da70
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 09:10:43 +0800

    docs:移除智能客服和服务列表路由，优化路由配置

--- alinesno-infra-base-authority-ui/src/router/index.js
+++ alinesno-infra-base-authority-ui/src/router/index.js
@@ -65,18 +65,6 @@ export const constantRoutes = [
         meta: { title: '首页', icon: 'dashboard', affix: true }
       },
       {
-        path: '/dashboard/smartService',
-        component: () => import('@/views/smartService'),
-        name: '/dashboard/smartService',
-        meta: { title: '智能客服', icon: 'dashboard', affix: true }
-      },
-      // {
-      //   path: '/dashboard/serviceList',
-      //   component: () => import('@/views/serviceList'),
-      //   name: '/dashboard/serviceList',
-      //   meta: { title: '服务列表', icon: 'dashboard', affix: true }
-      // },
-      {
         path: '/dashboard/suportTechnique',
         component: () => import('@/views/suportTechnique'),
         name: '/dashboard/suportTechnique',
